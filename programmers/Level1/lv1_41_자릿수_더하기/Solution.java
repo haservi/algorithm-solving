28d3e5f7a20a3afe557d6a8394857a1d0b4b68e2
Haseop Lee <haservi@naver.com>
2021-09-02 20:36:51 +0900

    Add [프로그래머스] 자릿수 더하기 문제 풀이

---
+++ programmers/Level1/lv1_41_자릿수_더하기/Solution.java
@@ -0,0 +1,29 @@
+package programmers.Level1.lv1_41_자릿수_더하기;
+
+import java.util.Stack;
+
+class Solution {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    int s = 123;
+
+    int result = solution.solution(s);
+    System.out.println("결과 : " + result);
+  }
+
+  public int solution(int n) {
+    Stack<Integer> stack = new Stack<Integer>();
+    int answer = 0;
+    while (n > 0) {
+      stack.push(n % 10);
+      n = n / 10;
+    }
+    while (!stack.isEmpty()) {
+      answer += stack.pop();
+    }
+
+    System.out.println(answer);
+    return answer;
+  }
+}
