d5a482c3186fb97767bdb5a970addda9ba16fc8a
Haseop Lee <haservi@naver.com>
2021-09-06 20:15:31 +0900

    Add [프로그래머스] 콜라츠 추측 문제 풀이

---
+++ programmers/Level1/lv1_46_콜라츠_추측/Solution.java
@@ -0,0 +1,28 @@
+package programmers.Level1.lv1_46_콜라츠_추측;
+
+class Solution {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    int num = 626331;
+
+    int result = solution.solution(num);
+    System.out.println("결과 : " + result);
+  }
+
+  public int solution(long num) {
+    int answer = 0;
+    while (num != 1) {
+      if (num % 2 == 0) {
+        num = num / 2;
+      } else {
+        num = (num * 3) + 1;
+      }
+      answer++;
+      if (answer == 500) {
+        return -1;
+      }
+    }
+    return answer;
+  }
+}
