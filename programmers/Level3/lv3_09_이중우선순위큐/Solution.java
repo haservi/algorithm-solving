dad726e793a72a30b394c5eef87302b47070e544
Haseop Lee <haservi@naver.com>
2022-02-18 19:51:09 +0900

    Add [프로그래머스] 이중우선순위큐 문제 추가

---
+++ programmers/Level3/lv3_09_이중우선순위큐/Solution.java
@@ -0,0 +1,17 @@
+package programmers.Level3.lv3_09_이중우선순위큐;
+
+class Solution {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    String[] operations = { "I 16", "D 1" };
+
+    int[] result = solution.solution(operations);
+    System.out.println("결과 : " + result);
+  }
+
+  public int[] solution(String[] operations) {
+    int[] answer = {};
+    return answer;
+  }
+}
