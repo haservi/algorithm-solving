3435929b27d611de758823fab30bde6f7bd35be5
Haseop Lee <haservi@naver.com>
2023-08-29 21:45:21 +0900

    Style 코드 줄 정리

--- programmers/Level3/solution/05_헤비_유저가_소유한_장소/Solution_mysql.sql
+++ programmers/Level3/solution/05_헤비_유저가_소유한_장소/Solution_mysql.sql
@@ -1,6 +1,3 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/77487
-
--- 코드를 입력하세요
 SELECT
   p.ID,
   p.NAME,
@@ -11,6 +8,6 @@ WHERE p.HOST_ID IN
   SELECT p_in.HOST_ID
   FROM PLACES AS p_in
   GROUP BY p_in.HOST_ID
-    HAVING COUNT(p_in.HOST_ID) > 1
+  HAVING COUNT(p_in.HOST_ID) > 1
 )
 
