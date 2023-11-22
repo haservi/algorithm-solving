80f9b5814b90a06ccaf3ceb7df2a086939d3aa4a
Haseop Lee <haservi@naver.com>
2023-11-22 22:43:27 +0900

    Feat [프로그래머스] 문자열 다루기 기본 문제 풀이(도서 참조)

--- programmers/Level1/solution/_24_문자열_다루기_기본/Solution.java
+++ programmers/Level1/solution/_24_문자열_다루기_기본/Solution.java
@@ -2,21 +2,26 @@ package programmers.Level1.solution._24_문자열_다루기_기본;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/12918
- * 1. 정규식을 이용하여 해결한다.
- * 2. 길이가 4, 6인 정수만 검색하는 정규식 구현
  */
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
         String s = "132234";
+        boolean expectedResult = true;
 
         boolean result = solution.solution(s);
-        System.out.println("결과 : " + result);
+        
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public boolean solution(String s) {
         boolean answer = true;
+        // 숫자로 구성된 4또는 6의 길이 정규식
         String regex = "^[0-9]{4}$|^[0-9]{6}$";
         if (s.matches(regex)) {
             answer = true;
@@ -24,5 +29,19 @@ class Solution {
             answer = false;
         }
         return answer;
+    }
+
+    public boolean solution2(String s) {
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
