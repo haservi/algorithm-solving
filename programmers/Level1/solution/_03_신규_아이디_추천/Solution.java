d050a89c6a0904ca59e2e8330cfe7ce458a00951
Haseop Lee <haservi@naver.com>
2023-11-23 18:51:58 +0900

    Feat [프로그래머스] 신규 아이디 추천 문제 풀이(도서 참조)

--- programmers/Level1/solution/_03_신규_아이디_추천/Solution.java
+++ programmers/Level1/solution/_03_신규_아이디_추천/Solution.java
@@ -2,15 +2,22 @@ package programmers.Level1.solution._03_신규_아이디_추천;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/72410
- * 1. 정규식을 이용해 풀이했다.
  */
 public class Solution {
 
     public static void main(String[] args) {
+
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        String expectedResult = "bat.y.abcdefghi";
+
         Solution solution = new Solution();
-        String result = solution.solution("...!@BaT#*..y.abcdefgh.ijklm");
-        // String result = solution.solution("");
-        System.out.println("결과: " + result);
+        String result = solution.solution(new_id);
+
+        if (expectedResult.equals(result)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public String solution(String new_id) {
@@ -54,6 +61,32 @@ public class Solution {
         String returnId = step7;
 
         return returnId;
+    }
+
+    public String solution2(String newId) {
+        // 1단계: 영어를 소문자로
+        newId = newId.toLowerCase();
+        // 2단계: 알파벳 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 문자 제거
+        newId = newId.replaceAll("[^a-z0-9\\-_.]", "");
+        // 3단계: 연속된 마침표를 하나의 마침표로 치환
+        newId = newId.replaceAll("\\.+", ".");
+        // 4단계: 처음이나 끝에 등장하는 마침표 제거
+        newId = newId.replaceAll("^\\.+|\\.+$", "");
+        // 5단계: 빈 문자열일 경우 "a" 대입
+        if (newId.isEmpty()) {
+            newId = "a";
+        }
+        // 6단계: 길이가 16자 이상이면 앞 15개의 문자만 남기고, 문자열이 .으로 끝난다면 이를 제거
+        if (16 <= newId.length() ) {
+            newId = newId.substring(0, 15);
+            newId = newId.replaceAll("\\.+$", "");
+        }
+        // 7단계: 문자열 길이가 3 이상이 될 때까지 마지막 문자 이어 붙이기
+        while (newId.length() < 3) {
+            newId += newId.charAt(newId.length() - 1);
+        }
+        
+        return newId;
     }
 
 }
