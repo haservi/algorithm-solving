52084e18f679a0b9172d96197057cb5c8c5bfeff
Haseop Lee <haservi@naver.com>
2023-11-29 21:37:43 +0900

    Feat [프로그래머스] 모의고사 문제 풀이(도서 참조)

--- programmers/Level1/solution/_41_모의고사/Solution.java
+++ programmers/Level1/solution/_41_모의고사/Solution.java
@@ -1,26 +1,27 @@
 package programmers.Level1.solution._41_모의고사;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42840
  */
 public class Solution {
     public static void main(String args[]) {
+        int[] answers = { 1, 3, 2, 4, 2 };
+        int[] expectedResult = { 1, 2, 3 };
 
         Solution solution = new Solution();
-
-        System.out.println("Test");
-        // int[] answers = { 1, 2, 3, 4, 5 };
-        // int[] answers = { 1, 3, 2, 4, 2 };
-        int[] answers = { 3, 3, 1, 3, 2, 4, 2 };
         int[] result = solution.solution(answers);
 
-        System.out.println("result = " + Arrays.toString(result));
+        if (Arrays.equals(expectedResult, result)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public int[] solution(int[] answers) {
-
         int[] user1Pattern = { 1, 2, 3, 4, 5 };
         int[] user2Pattern = { 2, 1, 2, 3, 2, 4, 2, 5 };
         int[] user3Pattern = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
@@ -56,4 +57,40 @@ public class Solution {
         return answer;
     }
 
+    private static final int[][] RULES = {
+            { 1, 2, 3, 4, 5 },
+            { 2, 1, 2, 3, 2, 4, 2, 5 },
+            { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 },
+    };
+
+    public int[] solution2(int[] answers) {
+        int[] corrects = new int[3];
+        int max = 0;
+
+        for (int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+
+            for (int person = 0; person < 3; person++) {
+                int picked = getPicked(person, problem);
+                if (answer == picked) {
+                    if (++corrects[person] > max) {
+                        max = corrects[person];
+                    }
+                }
+            }
+        }
+
+        // 람다 혹은 익명 메서드로 전달되는 메서드에서 외부 변수 참조 시에는 final 키워드 명시
+        final int maxCorrects = max;
+        return IntStream.range(0, 3)
+                .filter(i -> corrects[i] == maxCorrects)
+                .map(i -> i + 1)
+                .toArray();
+    }
+
+    private int getPicked(int person, int problem) {
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
 }
