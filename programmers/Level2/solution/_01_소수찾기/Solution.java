a9bc740c3627b4ba5b510f8c8389e17dab0341fd
Haseop Lee <haservi@naver.com>
2023-12-03 20:22:37 +0900

    Feat [프로그래머스] 소수찾기 문제 풀이(도서 참조)

--- programmers/Level2/solution/_01_소수찾기/Solution.java
+++ programmers/Level2/solution/_01_소수찾기/Solution.java
@@ -2,27 +2,36 @@
 package programmers.Level2.solution._01_소수찾기;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * 1. 값을 char에 담아서 반복적으로 소수를 찾도록 구현했다.
+ * https://programmers.co.kr/learn/courses/30/lessons/42839
  */
 class Solution {
+
+    public static void main(String[] args) {
+        String numbers = "011";
+        int expectedResult = 2;
+
+        Solution solution = new Solution();
+        int result = solution.solution(numbers);
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
 
     private TreeSet<String> set = new TreeSet<>();
     private int count;
 
-    public static void main(String[] args) {
-
-        String numbers = "011";
-        Solution s = new Solution();
-        int totalCount = s.solution(numbers);
-        System.out.println("만들 수 있는 소수 개수: " + totalCount);
-    }
-
+    /**
+     * 값을 char에 담아서 반복적으로 소수를 찾도록 구현
+     */
     public int solution(String numbers) {
-
         int size = numbers.length();
 
         // 리스트에 담아줌
@@ -43,7 +52,6 @@ class Solution {
     }
 
     public void permutation(List<Character> arr, List<Character> result, int n, int r) {
-
         if (r == 0) {
 
             // 0으로 시작하는 부분집합은 제외
@@ -79,7 +87,6 @@ class Solution {
     }
 
     private boolean isPrime(int n) {
-
         int i;
         int sqrt = (int) Math.sqrt(n);
 
@@ -97,6 +104,47 @@ class Solution {
                 return false;
         }
 
+        return true;
+    }
+
+    // ---------------------------------------------------------------
+    /**
+     * Set을 반환하지 않으며, isUsed를 이용해 사용하지 않은 숫자만 찾도록 함
+     */
+    public int solution2(String nums) {
+        Set<Integer> primes = new HashSet<>();
+        int[] numbers = nums.chars().map(c -> c - '0').toArray();
+        getPrimes(0, numbers, new boolean[numbers.length], primes);
+        return primes.size();
+    }
+
+    private void getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
+        if (isPrime2(acc)) {
+            primes.add(acc);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isUsed[i]) {
+                continue;
+            }
+
+            int nextAcc = acc * 10 + numbers[i];
+
+            isUsed[i] = true;
+            getPrimes(nextAcc, numbers, isUsed, primes);
+            isUsed[i] = false;
+        }
+    }
+
+    private boolean isPrime2(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
