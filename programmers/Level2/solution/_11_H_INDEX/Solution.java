3f531ac97077fa62d0373ca570e0c255ba644f21
Haseop Lee <haservi@naver.com>
2023-12-08 21:10:29 +0900

    Feat [프로그래머스] H-Index 문제 풀이(도서 참조)

--- programmers/Level2/solution/_11_H_INDEX/Solution.java
+++ programmers/Level2/solution/_11_H_INDEX/Solution.java
@@ -3,18 +3,26 @@ package programmers.Level2.solution._11_H_INDEX;
 import java.util.Arrays;
 
 /**
- * 1. 총 논문의 수 중 해당 논문의 인용한 값의 최대값을 구하는 문제이다.
- * 2. 정렬한 뒤에 전체 길이만큼 순회하면서 인용횟수가 크거나 같은 경우를 찾으면 H-Index값을 구할 수 있다.
+ * https://programmers.co.kr/learn/courses/30/lessons/42747
  */
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        int[] citations = { 3, 0, 6, 1, 5 };
+        int expectedResult = 3;
 
-        int[] citations = { 1, 1, 2, 3, 4 };
-        int result = solution.solution(citations);
-        System.out.println("결과: " + result);
+        Solution solution = new Solution();
+        int result = solution.solution2(citations);
+
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
+    /**
+     * 정렬한 뒤에 전체 길이만큼 순회하면서 인용횟수가 크거나 같은 경우
+     */
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
@@ -29,4 +37,20 @@ public class Solution {
         }
         return answer;
     }
+
+    public int solution2(int[] citations) {
+        Arrays.sort(citations);
+        for (int h = citations.length; h >= 1; h--) {
+            if (isValid(citations, h)) {
+                return h;
+            }
+        }
+        return 0;
+    }
+
+    private boolean isValid(int[] citations, int h) {
+        int index = citations.length - h;
+        return citations[index] >= h;
+    }
+
 }
