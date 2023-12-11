90474605116482aceab03bb5123fa8486ca5b237
Haseop Lee <haservi@naver.com>
2023-12-11 15:55:01 +0900

    Chore [프로그래머스] 문자열 내림차순으로 배치하기 문제 주석 코드 설명 추가

--- programmers/Level1/solution/_71_문자열_내림차순으로_배치하기/Solution.java
+++ programmers/Level1/solution/_71_문자열_내림차순으로_배치하기/Solution.java
@@ -20,6 +20,10 @@ public class Solution {
         return s.chars()
                 .boxed()
                 .sorted((v1, v2) -> v2 - v1)
+                // collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner)
+                // supplier: 반환할 객체 생성
+                // accumlator: 원소를 어떻게 누적할지 정의(appendCodePoint를 사용해야 문자열에 정수가 들어가지 않고 문자 형식으로 들어감)
+                // cobiner: 반환될 객체가 여러개 있을때 어떻게 합칠지 정의
                 .collect(StringBuilder::new,
                         StringBuilder::appendCodePoint,
                         StringBuilder::append)
