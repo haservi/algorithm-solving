2cd72cd94261ea34e51ec5dcf5c748b68b9e76a6
Haseop Lee <haservi@naver.com>
2023-12-13 22:23:56 +0900

    Feat [프로그래머스] 가장 큰 수 문제 풀이(도서 참조)

--- programmers/Level2/solution/_10_가장_큰_수/Solution.java
+++ programmers/Level2/solution/_10_가장_큰_수/Solution.java
@@ -2,15 +2,24 @@ package programmers.Level2.solution._10_가장_큰_수;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42746
+ */
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        int[] numbers = { 3, 30, 34, 5, 9 };
+        String expectedResult = "9534330";
 
-        // int[] numbers = { 3, 30, 34, 5, 9 };
-        int[] numbers = { 9, 5, 34, 3, 30 };
+        Solution solution = new Solution();
         String result = solution.solution(numbers);
-        System.out.println("결과: " + result);
+
+        if (expectedResult.equals(result)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public String solution(int[] numbers) {
@@ -27,18 +36,6 @@ public class Solution {
                 String aToB = o1 + o2;
                 String bToA = o2 + o1;
                 int result = bToA.compareTo(aToB);
-
-                // String resultMent = "";
-                // if (result == 0) {
-                //     resultMent = "same";
-                // } else if (result < 0) {
-                //     resultMent = "big";
-                // } else {
-                //     resultMent = "small";
-                // }
-
-                // System.out.println("compare: " + aToB + " vs " + bToA + " -> " + resultMent +
-                // ", result: " + o1 + " vs " + o2);
                 return result;
             };
         });
@@ -52,5 +49,20 @@ public class Solution {
         }
 
         return rlt.toString();
+    }
+
+    public String solution2(int[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> {
+                    // 순서를 바꾼 두 값을 더한 뒤 값이 클 경우 변경
+                    int original = Integer.parseInt(s1 + s2);
+                    int reversed = Integer.parseInt(s2 + s1);
+                    return reversed - original;
+                })
+                // 하나의 문자열로 합침
+                .collect(Collectors.joining(""))
+                // 0으로만 구성된 경우가 있으며, 문자열의 시작 부분에 0이 반복해서 나타난다면 0하나로 대체
+                .replaceAll("^0+", "0");
     }
 }
