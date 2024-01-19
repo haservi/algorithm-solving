c04edd89be15c1a1a02e2419f9633de6bf79b156
Haseop Lee <haservi@naver.com>
2024-01-19 22:07:19 +0900

    Feat [프로그래머스] 없는 숫자 더하기 문제 풀이(도서 참조)

--- programmers/Level1/solution/_44_없는_숫자_더하기/Solution.java
+++ programmers/Level1/solution/_44_없는_숫자_더하기/Solution.java
@@ -1,18 +1,25 @@
 package programmers.Level1.solution._44_없는_숫자_더하기;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/86051
  */
 public class Solution {
 
     public static void main(String[] args) {
-
         int[] numbers = { 5, 8, 4, 0, 6, 7, 9 };
+        int expectedResult = 6;
 
         Solution solution = new Solution();
-        int result = solution.solution(numbers);
+        int result = solution.solution2(numbers);
 
-        System.out.println(result);
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public int solution(int[] numbers) {
@@ -30,6 +37,22 @@ public class Solution {
         }
 
         return answer;
+    }
+
+    public int solution2(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int v : numbers) {
+            set.add(v);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            if (set.contains(i)) {
+                continue;
+            }
+            sum += i;
+        }
+        return sum;
     }
 
 }
