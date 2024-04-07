d430e4a780c4222df6229aa5c1451ea60d4fc97a
Haseop Lee <haservi@naver.com>
2024-04-07 20:24:23 +0900

    Feat [프로그래머스] 다리를 지나는 트럭 문제 풀이(도서 참조)

--- programmers/Level2/solution/_08_다리를_지나는_트럭/Solution.java
+++ programmers/Level2/solution/_08_다리를_지나는_트럭/Solution.java
@@ -4,23 +4,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 1. 트럭이 모두 다리를 건너가야 하기 때문에 트럭의 횟수만큼 순회한다.
- * 2. 다리를 지날 때는 트럭큐를 이용하며, 시간을 계속 추가한다.
- * 3. 마지막 트럭의 경우 지나갈 때 다리의 길이만큼 시간을 소모하기 때문에 처음에 다리의 길이만큼 값을 더해준다.
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42583
  */
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
         int bridge_length = 2;
         int weight = 10;
         int[] truck_weights = { 7, 4, 5, 6 };
+        int expectedResult = 8;
+
+        Solution solution = new Solution();
         int result = solution.solution(bridge_length, weight, truck_weights);
-        System.out.println("결과: " + result);
+
+        if (result == expectedResult) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
+    /**
+     * 1. 트럭이 모두 다리를 건너가야 하기 때문에 트럭의 횟수만큼 순회한다.
+     * 2. 다리를 지날 때는 트럭큐를 이용하며, 시간을 계속 추가한다.
+     * 3. 마지막 트럭의 경우 지나갈 때 다리의 길이만큼 시간을 소모하기 때문에 처음에 다리의 길이만큼 값을 더해준다.
+     */
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-
         Queue<Integer> truckQueue = new LinkedList<Integer>();
 
         int time = bridge_length;
@@ -48,6 +56,39 @@ public class Solution {
             }
 
         }
+        return time;
+    }
+
+    public int solution2(int bridge_length, int weight, int[] truck_weights) {
+        int bridgeWeight = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.add(0);
+        }
+
+        int time = 0;
+        int truckIndex = 0;
+        while (truckIndex < truck_weights.length) {
+            bridgeWeight -= bridge.poll();
+
+            int truckWeight = truck_weights[truckIndex];
+            if (bridgeWeight + truckWeight <= weight) {
+                bridge.add(truckWeight);
+                bridgeWeight += truckWeight;
+                truckIndex++;
+            } else {
+                bridge.add(0);
+            }
+
+            time++;
+        }
+
+        while (bridgeWeight > 0) {
+            bridgeWeight -= bridge.poll();
+            time++;
+        }
+
         return time;
     }
 }
