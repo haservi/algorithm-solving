8be51bbd2c74b29ac89ff50c72c27db0efaa5052
Haseop Lee <haservi@naver.com>
2024-05-10 19:46:15 +0900

    Feat [프로그래머스] 디스크 컨트롤러 문제 풀이(도서 참조)

--- programmers/Level3/solution/_05_디스크_컨트롤러/Solution.java
+++ programmers/Level3/solution/_05_디스크_컨트롤러/Solution.java
@@ -1,16 +1,27 @@
 package programmers.Level3.solution._05_디스크_컨트롤러;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42627
+ */
 class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
         int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
+        int expectedResult = 9;
 
-        int result = solution.solution(jobs);
-        System.out.println("결과 : " + result);
+        Solution solution = new Solution();
+        int result = solution.solution1(jobs);
+
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public int solution(int[][] jobs) {
@@ -44,5 +55,46 @@ class Solution {
         }
 
         return time / jobs.length;
+    }
+
+    public int solution1(int[][] jobData) {
+        Job[] jobs = new Job[jobData.length];
+        for (int i = 0; i < jobData.length; i++) {
+            jobs[i] = new Job(jobData[i][0], jobData[i][1]);
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.start));
+
+        // 요청 시간이 짧은 일감부터 정렬
+        Queue<Job> q = new LinkedList<>(Arrays.asList(jobs));
+        // 걸리는 시간이 짧은 일감부터 정렬
+        PriorityQueue<Job> pq = new PriorityQueue<>(Comparator.comparingInt(job -> job.duration));
+
+        int exec = 0;
+        int time = 0;
+        while (!q.isEmpty() || !pq.isEmpty()) {
+            while (!q.isEmpty() && q.peek().start <= time) {
+                pq.add(q.poll());
+            }
+
+            if (pq.isEmpty()) {
+                time = q.peek().start;
+                continue;
+            }
+
+            Job job = pq.poll();
+            exec += time + job.duration - job.start;
+            time += job.duration;
+        }
+        return exec / jobs.length;
+    }
+
+    private static class Job {
+        private final int start;
+        private final int duration;
+
+        private Job(int start, int duration) {
+            this.start = start;
+            this.duration = duration;
+        }
     }
 }
