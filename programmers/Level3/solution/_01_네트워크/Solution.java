c3720441f469ad7601cc8bcd04aa06dd493c8d0c
Haseop Lee <haservi@naver.com>
2024-05-20 22:34:53 +0900

    Feat [프로그래머스] 네트워크 문제 풀이(도서 참조)

--- programmers/Level3/solution/_01_네트워크/Solution.java
+++ programmers/Level3/solution/_01_네트워크/Solution.java
@@ -1,22 +1,34 @@
 package programmers.Level3.solution._01_네트워크;
 
+import java.util.Stack;
+
 /**
- * https://programmers.co.kr/learn/courses/30/lessons/43162
- * 1. 재귀적으로 반복하며 중복되는 네트워크를 찾는 함수를 실행한다.
- * 2. n:n의 경우는 항상 true이며 그렇지 않은경우 false로 네트워크를 상태를 확인한다.
+ * https://programmers.co.kr/learn/courses/30/lessons/43162?language=java
  */
 class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
         int n = 3;
-        int[][] computers = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } }; // 2
-        // int[][] computers1 = { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } }; // 1
+        int[][] computers = {
+                { 1, 1, 0 },
+                { 1, 1, 0 },
+                { 0, 0, 1 }
+        };
+        int expectedResult = 2;
 
-        int result = solution.solution(n, computers);
-        System.out.println("결과 : " + result);
+        Solution solution = new Solution();
+        int result = solution.solution1(n, computers);
+
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
+    /**
+     * 1. 재귀적으로 반복하며 중복되는 네트워크를 찾는 함수를 실행한다.
+     * 2. n:n의 경우는 항상 true이며 그렇지 않은경우 false로 네트워크를 상태를 확인한다.
+     */
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
@@ -41,4 +53,41 @@ class Solution {
             }
         }
     }
+
+    public int solution1(int n, int[][] computers) {
+        boolean[] isVisited = new boolean[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (isVisited[i]) {
+                continue;
+            }
+            visitAll(i, computers, isVisited);
+            answer++;
+        }
+
+        return answer;
+    }
+
+    private void visitAll(int computer, int[][] computers, boolean[] isVisited) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(computer);
+
+        while (!stack.isEmpty()) {
+            int c = stack.pop();
+
+            if (isVisited[c]) {
+                continue;
+            }
+            isVisited[c] = true;
+
+            for (int next = 0; next < computers[c].length; next++) {
+                if (computers[c][next] == 0) {
+                    continue;
+                }
+                stack.push(next);
+            }
+        }
+    }
+
 }
