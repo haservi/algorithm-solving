f4a420bdd217e1589b4bc2c8296bd6fbc34a7b50
Haseop Lee <haservi@naver.com>
2024-05-23 20:15:14 +0900

    Feat [프로그래머스] 체육복 문제 풀이(도서 참조)

--- programmers/Level1/solution/_04_체육복/Solution.java
+++ programmers/Level1/solution/_04_체육복/Solution.java
@@ -1,18 +1,26 @@
 package programmers.Level1.solution._04_체육복;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
- * https://programmers.co.kr/learn/courses/30/lessons/42862
+ * https://programmers.co.kr/learn/courses/30/lessons/42862?language=java
  */
 public class Solution {
     public static void main(String[] args) {
+        int n = 5;
+        int[] lost = { 2, 4 };
+        int[] reserve = { 2, 3 };
+        int expectedResult = 5;
+
         Solution solution = new Solution();
+        int result = solution.solution1(n, lost, reserve);
 
-        int totalStudent = 5;
-        int[] lostStudents = { 4, 5 };
-        int[] reserveStudents = { 1, 4 };
-
-        int result = solution.solution(totalStudent, lostStudents, reserveStudents);
-        System.out.println("결과: " + result + "명");
+        if (expectedResult == result) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
     }
 
     public int solution(int n, int[] lost, int[] reserve) {
@@ -51,6 +59,50 @@ public class Solution {
         }
 
         return answer;
+    }
+
+    // --------------------------------------------------------------
+
+    public int solution1(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        // lost, reserve을 set으로 변환하여 Set의 교집합 구함
+        Set<Integer> owns = Arrays.stream(lost)
+                .boxed()
+                .collect(Collectors.toSet());
+        owns.retainAll(Arrays.stream(reserve)
+                .boxed()
+                .collect(Collectors.toSet()));
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int l : lost) {
+            q.add(l);
+        }
+
+        int get = 0;
+        for (int r : reserve) {
+            // 체육복을 가지고 있지만 체육복을 잃어버린 경우
+            if (owns.contains(r)) {
+                continue;
+            }
+
+            // queue는 비어있지 않고, 너무 멀리있거나 여벌옷이 있으면 빌릴 필요 없음
+            while (!q.isEmpty() && (q.peek() < r - 1 || owns.contains(q.peek()))) {
+                q.poll();
+            }
+
+            if (q.isEmpty()) {
+                break;
+            }
+
+            if (q.peek() <= r + 1) {
+                q.poll();
+                get++;
+            }
+        }
+
+        return n - lost.length + owns.size() + get;
     }
 
 }
