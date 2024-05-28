210748c6569af49fc05e1545b7d14dc8fe91d239
Haseop Lee <haservi@naver.com>
2024-05-28 20:48:20 +0900

    Feat [프로그래머스] 신고 결과 받기 문제 풀이(도서 참조)

--- programmers/Level1/solution/_42_신고_결과_받기/Solution.java
+++ programmers/Level1/solution/_42_신고_결과_받기/Solution.java
@@ -1,27 +1,30 @@
 package programmers.Level1.solution._42_신고_결과_받기;
 
 import java.util.*;
+import java.util.stream.*;
 
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java
  */
 class Solution {
     public static void main(String[] args) {
+        String[] id_list = { "muzi", "frodo", "apeach", "neo" };
+        String[] report = { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi" };
+        int k = 2;
+        int[] expectedResult = { 2, 1, 1, 0 };
 
         Solution solution = new Solution();
+        int[] result = solution.solution1(id_list, report, k);
 
-        String[] id_list = { "con", "ryan" };
-        String[] report = { "ryan con", "ryan con", "ryan con", "ryan con" };
-        int k = 3;
-
-        int[] result = solution.solution(id_list, report, k);
-
-        System.out.println("result = " + Arrays.toString(result));
+        if (Arrays.equals(expectedResult, result)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
 
     }
 
     public int[] solution(String[] id_list, String[] reports, int k) {
-
         int[] answer = new int[id_list.length];
         Map<String, HashSet<String>> reporterInfoMap = new HashMap<>();
         Map<String, Integer> reportedCountMap = new HashMap<>();
@@ -47,5 +50,41 @@ class Solution {
             }
         }
         return answer;
+    }
+
+    public int[] solution1(String[] id_list, String[] report, int k) {
+        Map<String, Set<String>> reports = new HashMap<>();
+        for (String id : id_list) {
+            reports.put(id, new HashSet<>());
+        }
+
+        Map<String, Integer> reported = new HashMap<>();
+
+        // 신고자가 신고한 사람 넣기
+        for (String r : report) {
+            String[] tokens = r.split(" ");
+            String reporter = tokens[0];
+            String target = tokens[1];
+
+            // 사용자 명단에 신고자 리스트를 넣음("muzi frodo"인 경우 frodo에 muzi를 넣음)
+            Set<String> set = reports.get(reporter);
+            if (set.contains(target)) {
+                continue;
+            }
+
+            set.add(target);
+            reported.putIfAbsent(target, 0);
+            reported.put(target, reported.get(target) + 1);
+        }
+
+        Set<String> banned = reported.keySet().stream()
+                .filter(id -> reported.get(id) >= k)
+                .collect(Collectors.toSet());
+
+        return Arrays.stream(id_list)
+                .mapToInt(id -> (int) reports.get(id).stream()
+                        .filter(banned::contains)
+                        .count())
+                .toArray();
     }
 }
