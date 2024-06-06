50e73a1052480b9cfecc786c1887826c25709ce8
Haseop Lee <haservi@naver.com>
2024-06-06 17:30:29 +0900

    Feat [프로그래머스] 운영체제 문제 풀이(도서 참조)

--- programmers/pccp/README.md
+++ programmers/pccp/README.md
@@ -7,3 +7,4 @@
 | 01 |                    | [외톨이 알파벳](https://school.programmers.co.kr/learn/courses/20847/lessons/255900) | [Java](./solution/_01_외톨이_알파벳/Solution.java) |
 | 02 |                    | [체육대회](https://school.programmers.co.kr/learn/courses/15008/lessons/121684) | [Java](./solution/_02_체육대회/Solution.java) |
 | 03 |                    | [유전 법칙](https://school.programmers.co.kr/learn/courses/15008/lessons/121685) | [Java](./solution/_03_유전_법칙/Solution.java) |
+| 04 |                    | [운영체제](https://school.programmers.co.kr/learn/courses/15008/lessons/121686) | [Java](./solution/_04_운영체제/Solution.java) |
---
+++ programmers/pccp/solution/_04_운영체제/Solution.java
@@ -0,0 +1,75 @@
+package programmers.pccp.solution._04_운영체제;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/15008/lessons/121686?language=java
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[][] program = {{2, 0, 10}, {1, 5, 5}, {3, 5, 3}, {3, 12, 2}}
+        long[] expectedResult = { 20, 5, 0, 16, 0, 0, 0, 0, 0, 0, 0 };
+
+        Solution solution = new Solution();
+        long[] result = solution.solution(program);
+
+        if (Arrays.equals(expectedResult, result)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+
+    public long[] solution(int[][] program) {
+
+        Queue<Program> programs = Arrays.stream(program)
+                .map(p -> new Program(p[0], p[1], p[2]))
+                .sorted(Comparator.comparingInt(p -> p.calledAt))
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        PriorityQueue<Program> pq = new PriorityQueue<>((a, b) -> {
+            if (a.priority != b.priority) {
+                return a.priority - b.priority;
+            }
+            return a.calledAt - b.calledAt;
+        });
+
+        long[] answer = new long[11];
+        int time = 0;
+
+        while (!programs.isEmpty() || !pq.isEmpty()) {
+            while (!programs.isEmpty() && programs.peek().calledAt <= time) {
+                pq.add(programs.poll());
+            }
+
+            if (pq.isEmpty()) {
+                time = programs.peek().calledAt;
+                continue;
+            }
+
+            Program p = pq.poll();
+            answer[p.priority] += time - p.calledAt;
+            time += p.executionTime;
+        }
+        answer[0] = time;
+        return answer;
+    }
+
+    private static class Program {
+        private final int priority;
+        private final int calledAt;
+        private final int executionTime;
+
+        public Program(int priority, int calledAt, int executionTime) {
+            this.priority = priority;
+            this.calledAt = calledAt;
+            this.executionTime = executionTime;
+        }
+    }
+}
